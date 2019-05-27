4887300138c6fd7f269cfac063b15f6020996899
Edycar Reyes <edycarreyes@gmail.com>
2019-05-27 17:28:03 -0600

    se crea el routing para el componente principal que seria el login dentro del modulo de routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-const routes: Routes = [];
+// importaciones de los componentes
+import {LoginComponent} from './pages/login/login.component';
+
+
+const routes: Routes = [
+  {path: '', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
