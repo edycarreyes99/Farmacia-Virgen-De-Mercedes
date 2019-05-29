83f3f25e15e5d46527d2be4ba14430555e11549b
Edycar Reyes <edycarreyes@gmail.com>
2019-05-29 15:20:58 -0600

    se agregan las tablas de productos vendidos en el dia y en la semana se diseñan correctamente y se le agregan los datos de prueba

--- src/app/pages/estadisticas/estadisticas.component.ts
+++ src/app/pages/estadisticas/estadisticas.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+export interface Transaction {
+  Producto: string;
+  Cantidad: number;
+  Valor: number;
+}
 
 @Component({
   selector: 'app-estadisticas',
@@ -7,7 +13,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstadisticasComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['Producto', 'Cantidad', 'Valor'];
+  transactions: Transaction[] = [
+    {Producto: 'Beach ball', Cantidad: 4, Valor: 4},
+    {Producto: 'Towel', Cantidad: 5, Valor: 5},
+    {Producto: 'Frisbee', Cantidad: 2, Valor: 2},
+    {Producto: 'Sunscreen', Cantidad: 4, Valor: 4},
+    {Producto: 'Cooler', Cantidad: 25, Valor: 25},
+    {Producto: 'Swim suit', Cantidad: 15, Valor: 15},
+  ];
+
+  /** Gets the total Valor of all transactions. */
+  getTotalValor() {
+    return this.transactions.map(t => t.Valor).reduce((acc, value) => acc + value, 0);
+  }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
